4d421b9bb0397f33604dfd85eff6f87f44ac2d7a
Absar <tuhiner09@gmail.com>
2026-08-16 16:15:46 +0600

    Module_3 class_2 Follow.cs added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Follow
+    {
+        private Guid _id;
+        private Guid _followerId;
+        private Guid _followingId;
+        private DateTime _followedAt;
+
+        public Follow()
+        {
+            _id = Guid.NewGuid();
+
+        }
+        public Guid FollowerId { get; set; }
+        public DateTime FollowedAt { get; set; }
+        public Guid followingId { get; set; }
+    }
+}
