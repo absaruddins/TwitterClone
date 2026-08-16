a230780c61db2e629ff818867c9d0456222661db
Absar <tuhiner09@gmail.com>
2026-08-16 15:56:50 +0600

    Module_3 class_2 Tweet.cs added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        private Guid _id;
+        private Guid _autherId;
+        private string _content;
+        private DateTime _createdAt;
+        private DateTime _updatedAt;
+        private Guid _createdBy;
+        private Guid _modifiedBy;
+
+        public Tweet()
+        {
+            _id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public Guid AutherId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+
+        
+
+    }
+}
