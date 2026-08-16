07a737d383fb12849368ec8db90debdedb5d415e
Absar <tuhiner09@gmail.com>
2026-08-16 16:42:22 +0600

    Module_3 class_2 Bookmark.cs added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Bookmark
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _createdAt;
+
+        public Bookmark()
+        {
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+        }
+        public Guid Id { get; }
+        public Guid UserId { get; set; }
+        public Guid TweetId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
