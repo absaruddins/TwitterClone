26446c424ae6c0d4c9cf55f19183e8b39a31eceb
Absar <tuhiner09@gmail.com>
2026-08-16 16:31:21 +0600

    Module_3 class_2 Notification.cs added

--- TwitterClone/TwitterClone.Domain/Entities/Notification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -4,27 +4,29 @@ using System.Text;
 
 namespace TwitterClone.Domain.Entities
 {
-    public class Retweet
+    public class Notification
     {
         private Guid _id;
         private Guid _userId;
+        private string _type;
+        private bool _isRead;
         private DateTime _createdAt;
         private DateTime _updatedAt;
         private Guid _createdBy;
         private Guid _modifiedBy;
-
-        public Retweet()
+        public Notification()
         {
             _id = Guid.NewGuid();
             _createdAt = DateTime.UtcNow;
+            _isRead = false;
         }
         public Guid Id { get; }
         public Guid UserId { get; set; }
+        public String Type { get; set; }
+        public bool IsRead { get; set; }
         public DateTime CreatedAt { get; set; }
         public Guid CreatedBy { get; set; }
-        public DateTime UpdatedAt  { get; set;}
+        public DateTime UpdatedAt { get; set; }
         public Guid UpdatedBy { get; set; }
     }
-
-}
 }
