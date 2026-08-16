e4d6b2b2c3f24aca3f0add08e2c171b9e2b42891
Absar <tuhiner09@gmail.com>
2026-08-16 15:01:08 +0600

    Module_3 class_2 User.cs added

--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace TwitterClone.Domain.Entities
 {
-    internal class User
+    public class User
     {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _createdAt;
+        private DateTime _updatedAt;
+        private Guid _createdBy;
+        private Guid _modifiedBy;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+
+        }
+        public Guid Id
+        {
+            get { return _id ; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName ; }
+            set { _firstName = value ; }
+        }
+        public string LastName
+        {
+            get { return _lastName ; }
+            set { _lastName = value ; }
+
+        }
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = value ; }
+
+
+        }
+        public DateTime UpdatedAt
+        {
+            get { return _updatedAt; }
+            set { _updatedAt = value  ; }
+
+        }
+        private Guid CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value ; }
+        }
+        private Guid ModifieddBy
+        {
+            get { return _modifiedBy; }
+            set { _modifiedBy = value; }
+        }
     }
 }
